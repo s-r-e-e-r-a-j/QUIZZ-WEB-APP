caf16c7ce4d38589b85bb51fc2963bd98d228bb9
C0DEGamer <154326486+C0DEGamer@users.noreply.github.com>
2024-08-05 18:31:55 +0530

    Add files via upload

---
+++ QUIZZ WEB APP/script.js
@@ -0,0 +1,453 @@
+			
+var questions=[
+
+    {
+        question:"which is the national bird of india",
+        answer: [
+       {option:  "peacock",systemitis:true},
+       {option: "tiger",systemitis:false},
+       {option: "elephant",systemitis:false},
+       {option: "wolf",systemitis:false},
+
+
+
+            ]
+    },
+    {
+        question:"which is the longest river in the world",
+        answer:[
+            {option:"nile",systemitis:true},
+            {option:"greatganga",systemitis:false},
+            {option:"amazon",systemitis:false},
+            {option:"niger",systemitis:false},
+            
+            ]
+
+    },
+    {
+        question:"which is the largest ocean in the world",
+        answer:[
+        {option:"indian ocean",systemitis:false},
+        {option:"arctic ocean",systemitis:false},
+        {option:"pacific ocean",systemitis:true},
+        {option:"atlantic ocean",systemitis:false},
+        
+        
+        ]
+
+        },
+        {
+            question:"Which is the indias first super computer",
+            answer:[
+
+           {option:"param800",systemitis:false},
+           {option:"param8",systemitis:false},
+           {option:"param80000",systemitis:false},
+           {option:"param8000",systemitis:true},
+
+
+                ]
+        },{
+            question:"which is largest animal of land",
+            answer:[
+            {option:"Elephant",systemitis:false},
+            {option:"African Elephant",systemitis:true},
+            {option:"tiger",systemitis:false},
+            {option:"crocodile",systemitis:false},
+
+                ]
+        },
+        {
+            question:"which is largest island in the world",
+            answer:[
+
+           {option:"New guinea",systemitis:false},
+           {option:"Green land",systemitis:true},
+           {option:"Andaman Nicobar",systemitis:false},
+           {option:"Hawaii",systemitis:false},
+
+                ]
+        },
+        {
+            question:"Which is the largest flower in the World",
+            answer:[
+           {option:"Jasmine",systemitis:false},
+           {option:"Ballon Flower",systemitis:false},
+           {option:"Rafflesia",systemitis:true},
+           {option:"Carmellia",systemitis:false},
+
+
+
+                ]
+        },
+        {
+        question:"Tsunami is a word in which language",
+        answer:[
+          {option:"Japanese",systemitis:true},
+          {option:"Urdu",systemitis:false},
+          {option:"French",systemitis:false},
+          {option:"Hindi",systemitis:false},
+
+            ]
+    },
+    {
+        question:"Which state has the longest coastal line in india",
+        answer:[
+              {option:"Kerala",systemitis:false},
+          {option:"Karnataka",systemitis:false},
+          {option:"Gujarat",systemitis:true},
+          {option:"West bengal",systemitis:false},
+
+        ]
+    },
+     {
+        question:"Which continent is known as Dark Continent",
+        answer:[
+              {option:"Australia",systemitis:false},
+          {option:"Africa",systemitis:true},
+          {option:"South America",systemitis:false},
+          {option:"Asia",systemitis:false},
+
+        ]
+    },
+     {
+        question:"Which is the smallest City in the World?",
+        answer:[
+              {option:"Vatican City",systemitis:true},
+          {option:"Bhutan",systemitis:false},
+          {option:"Nepal",systemitis:false},
+          {option:"Shri Lanka",systemitis:false},
+
+        ]
+    },
+     {
+        question:"Which is the Highest Dam of India",
+        answer:[
+              {option:"Rihand Dam",systemitis:false},
+          {option:"Mettur Dam",systemitis:false},
+          {option:"Tehri Dam",systemitis:true},
+          {option:"Sardar Sarovar Dam",systemitis:false},
+
+        ]
+    },{
+        question:"Which is the largest coffee-producing state of India?",
+        answer:[
+              {option:"Karnataka",systemitis:true},
+          {option:"TamilNadu",systemitis:false},
+          {option:"Kerala",systemitis:false},
+          {option:"Arunachal Pradesh",systemitis:false},
+
+        ]
+    },
+    {
+        question:"Which state has the largest area?",
+        answer:[
+              {option:"Mharashtra",systemitis:false},
+          {option:"Madhya Pradesh",systemitis:false},
+          {option:"UttarPradesh",systemitis:false},
+          {option:"Rajasthan",systemitis:true},
+
+        ]
+    },
+    {
+        question:"In what state ie Elephant falls located?",
+        answer:[
+              {option:"Manipur",systemitis:false},
+          {option:"Meghalaya",systemitis:true},
+          {option:"Orissa",systemitis:false},
+          {option:"Mizoram",systemitis:false},
+
+        ]
+    },
+    {
+        question:"Which state of india celebrates hunter holi,an 800-year tradition?",
+        answer:[
+              {option:"Delhi",systemitis:false},
+          {option:"Haryana",systemitis:true},
+          {option:"Uttar Pradesh",systemitis:false},
+          {option:"Punjab",systemitis:false},
+
+        ]
+    },
+    {
+        question:"Yakshagana is the folk dance of which state?",
+        answer:[
+              {option:"Karnataka",systemitis:true},
+          {option:"Kerala",systemitis:false},
+          {option:"Telangana",systemitis:false},
+          {option:"TamilNadu",systemitis:false},
+
+        ]
+    },
+    {
+        question:"What is the name of indias national aquatic animal?",
+        answer:[
+              {option:"River dolphin",systemitis:true},
+          {option:"Crocodile",systemitis:false},
+          {option:"Katla Fish",systemitis:false},
+          {option:"Green Frog",systemitis:false},
+
+        ]
+    },
+    {
+        question:"What is the staple drink of goa?",
+        answer:[
+              {option:"Toddy",systemitis:false},
+          {option:"Thandai",systemitis:false},
+          {option:"Sattu Sharbat",systemitis:false},
+          {option:"Feni",systemitis:true},
+
+        ]
+    },
+    {
+        question:"Which is the Indias total area?",
+        answer:[
+              {option:"3.7 Million sq.km",systemitis:false},
+          {option:"3.3 Million sq.km",systemitis:true},
+          {option:"4.2 Million sq.km",systemitis:false},
+          {option:"3.0 Million sq.km",systemitis:false},
+
+        ]
+    },
+    {
+        question:"When was the international monetary fund established?",
+        answer:[
+              {option:"1945",systemitis:true},
+          {option:"1946",systemitis:false},
+          {option:"1950",systemitis:false},
+          {option:"1947",systemitis:false},
+
+        ]
+    },
+    {
+        question:"Which is the major mineral found in punjab?",
+        answer:[
+              {option:"Coal",systemitis:false},
+          {option:"Salt",systemitis:true},
+          {option:"Iron",systemitis:false},
+          {option:"Gold",systemitis:false},
+
+        ]
+    },{
+        question:"Wen was shakespeare born?",
+        answer:[
+              {option:"1618 AD",systemitis:false},
+          {option:"156 AD",systemitis:true},
+          {option:"1642 AD",systemitis:false},
+          {option:"1776 AD",systemitis:false},
+
+        ]
+    },
+    {
+        question:"Which country hosted the first asian games?",
+        answer:[
+              {option:"India",systemitis:true},
+          {option:"Indonesia",systemitis:false},
+          {option:"Iran",systemitis:false},
+          {option:"Malaysia",systemitis:false},
+
+        ]
+    },
+    {
+        question:"Which minerals are found in Chattisgarh?",
+        answer:[
+              {option:"Iron-ore,limestone,dolomite",systemitis:false},
+          {option:"Coal,bauxite,corundum",systemitis:false},
+          {option:"Gaunet,gaurtz,marble,diamond",systemitis:false},
+          {option:"All of the above",systemitis:true},
+
+        ]
+    },
+    {
+        question:"Which indian satellite had a main function to perform x-ray experiments in space and send back data to earth?",
+        answer:[
+              {option:"None of the above",systemitis:false},
+          {option:"Bhaskara ll",systemitis:false},
+          {option:"Aryabhatta",systemitis:true},
+          {option:"Bhaskara 1L",systemitis:false},
+
+        ]
+    },{
+        question:"Which is the indias largest library with a collection of over 190 million volumes of reading and information material?",
+        answer:[
+              {option:"National library",systemitis:true},
+          {option:"National book trust",systemitis:false},
+          {option:"National educational resource centre",systemitis:false},
+          {option:"None of the above",systemitis:false},
+
+        ]
+    },{
+        question:"Which amongst the following has the lowest metabolic rate of oxygen consumption?",
+        answer:[
+              {option:"Horse",systemitis:false},
+          {option:"Dog",systemitis:false},
+          {option:"Elephant",systemitis:true},
+          {option:"Man",systemitis:false},
+
+        ]
+    },{
+        question:"Which country gifted the statue of liberty to usa in 1886?",
+        answer:[
+              {option:"England",systemitis:false},
+          {option:"Canada",systemitis:false},
+          {option:"Brazil",systemitis:false},
+          {option:"France",systemitis:true},
+
+        ]
+    },{
+        question:" In Which ocean 'bermuda Triangle, region is located?",
+        answer:[
+              {option:"Pacific",systemitis:false},
+          {option:"Atlantic",systemitis:true},
+          {option:"Artic",systemitis:false},
+          {option:"Indian",systemitis:false},
+
+        ]
+    }
+
+              //you can add more questions in this format
+        ]
+    
+    var timer;
+const questionout=document.querySelector("#question");
+const answerout=document.querySelector("#answerbutton");
+const nextbtn=document.querySelector("#next-btn");
+const counter=document.querySelector("#timer") 
+var speech=new SpeechSynthesisUtterance();
+speech.lang="en-US"||"en-GB"||"en-UK"
+speech.text=""
+speech.volume=1;
+speech.rate=0.8;
+speech.pitch=0.8;
+var questionindex=0;
+var score=0;
+
+var startquiz=()=>{
+
+questionindex=0;
+score=0;
+nextbtn.innerHTML="Next";
+showquestion()
+
+} 
+
+var showquestion=()=>{
+	timer=30;
+reset();
+let currentquestion=questions[questionindex];
+let questionno=questionindex + 1;
+questionout.innerHTML=questionno+"."+currentquestion.question;
+
+currentquestion.answer.forEach((ans)=>{
+
+const button=document.createElement("button");
+button.innerHTML=ans.option;
+button.setAttribute("class","btn");
+answerout.appendChild(button);
+
+if(ans.systemitis){
+
+button.dataset.systemitis=ans.systemitis
+
+}
+
+button.addEventListener("click",event=>{
+
+let selectedbtn=event.target;
+let iscorrect=selectedbtn.dataset.systemitis==="true";
+
+if(iscorrect){
+
+selectedbtn.classList.add("correct")
+score++
+
+speech.text="How Is It Possible it is a correct answer";
+window.speechSynthesis.speak(speech)
+
+}else{
+
+selectedbtn.classList.add("incorrect")
+speech.text="That is a Incorrect answer"
+window.speechSynthesis.speak(speech)
+
+}
+
+Array.from(answerout.children).forEach(button=>{
+
+if(button.dataset.systemitis==="true"){
+
+button.classList.add("correct")
+}
+button.disabled=true;
+})
+nextbtn.style.display="block"
+})
+
+
+})
+
+}  
+
+let reset=()=>{
+nextbtn.style.display="none"
+
+while(answerout.firstChild){
+  answerout.removeChild(answerout.firstChild)
+}
+}
+
+let showscore=()=>{
+reset();
+questionout.innerHTML=`you scored ${score} out of ${questions.length}`;
+speech.text=`you scored ${score} out of ${questions.length}`
+window.speechSynthesis.speak(speech)
+nextbtn.innerHTML="play again"
+nextbtn.style.display="block"
+}
+
+let nextbuttonactive=()=>{
+
+questionindex++
+if(questionindex<questions.length){
+showquestion()
+}else{
+showscore()
+}
+
+}
+
+nextbtn.addEventListener("click",()=>{
+
+if(questionindex<questions.length){
+   nextbuttonactive()
+}else{
+   startquiz()
+}
+
+})
+
+setInterval(()=>{
+timer-=1;
+counter.innerHTML=timer;
+if(timer===0){
+	questionindex++;
+	counter.innerHTML=timer;
+	reset();
+	showquestion();
+	
+}
+if(questionindex>=questions.length){
+		reset();
+		nextbtn.style.display="block"
+
+		questionout.innerHTML=`YOUR SCORE IS ${score} OUT OF ${questions.length}`;
+		speech.text=`YOUR SCORE IS ${score} OUT OF ${questions.length}`;
+		window.speechSynthesis.speak(speech)
+		nextbtn.innerHTML="play again"
+
+}
+
+
+},1000);
+
+startquiz()
